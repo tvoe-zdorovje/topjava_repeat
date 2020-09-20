d519083f8d1b2449c3131bb346ffa0f3f4418b75
tvoe-zdorovje <dr0sha98@gmail.com>
2020-09-20 14:35:10 +0300

    Implement Meal Service

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,37 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.*;
 
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(Integer userId, Meal meal) {
+        return repository.save(userId, meal);
+    }
+
+    public Meal get(int userId, int id) {
+        return checkNotFoundWithId(repository.get(userId, id), id);
+    }
+
+    public List<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+    public void update(int userId, Meal meal) {
+        checkNotFoundWithId(repository.save(userId, meal), meal.getId());
+    }
+
+    public void delete(int userId, int id) {
+        checkNotFoundWithId(repository.delete(userId, id), id);
+    }
 }
